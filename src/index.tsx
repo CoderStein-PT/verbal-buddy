0d6b935095f7f2b0de1a2e880bedab9f4d334b08
Max <mymaksym@gmail.com>
2023-01-13 09:19:26 +0000

    add cookies banner and analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import 'react-toastify/dist/ReactToastify.min.css'
 import 'react-tooltip/dist/react-tooltip.css'
+import './ext/glowCookies/glowCookies.css'
 import './styles/custom.css'
 import { App } from 'app'
 
