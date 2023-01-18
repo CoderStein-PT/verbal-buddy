00bdc7aba307edc9ac5eada1989cff386c7a41d4
Max <mymaksym@gmail.com>
2023-01-18 19:40:58 +0000

    voice fix

--- src/utils/use-setup-voice.tsx
+++ src/utils/use-setup-voice.tsx
@@ -11,12 +11,13 @@ export const useSetupVoice = () => {
     window.speechSynthesis.onvoiceschanged = () => {
       const voices = getVoices()
       useVoiceStore.setState({ voices })
-      const voiceURI = voices.find(
-        (v) =>
-          v.voiceURI ===
-            (voice || 'Google US English' || 'English United States') ||
-          !!v.default
-      )
+
+      const voiceURI =
+        voices.find(
+          (v) =>
+            v.voiceURI ===
+            (voice || 'Google US English' || 'English United States')
+        ) || voices.find((v) => !!v.default)
 
       if (!voiceURI) return
 
